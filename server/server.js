fe0011b1e7c9b1f430a1aa581368df3d505d39e2
Mark Piercey <markpiercey@Marks-iMac.local>
2020-03-23 23:03:16 -0230

    updated link to johns hopkins data

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const csv = require('csv-parser');
 const superagent  = require('superagent')
 const dataDir = './dist/data'
 const confirmedFile = 'confirmed.csv'
-const data = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+const data = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 //TODO: put this somewhere else
 function getLatestData() {
